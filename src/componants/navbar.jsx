56727237ed3abf46c3f0d284b74f1322ed0dd075
tajulrakibtaz <tajulrakibtaz@gmail.com>
2026-04-17 17:04:38 +0600

    added the icon for home page in navbar

--- src/componants/navbar.jsx
+++ src/componants/navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaHome } from 'react-icons/fa';
 import { Ri24HoursFill } from 'react-icons/ri';
 import { Link, NavLink } from 'react-router';
 const Navbar = () => {
@@ -6,10 +7,21 @@ const Navbar = () => {
         <div className='flex justify-between'>
             <div><samp className='text-black'>Keen</samp><samp className='text-emerald-800'>Keeper</samp></div>
             <div className='space-x-2'>
-                <button >
-     {/* <NavLink </NavLink> */}
-     <NavLink to={'/'} className={(prop)=>`${prop.isActive?'bg-green-900 rounded-md p-3  text-amber-50':'rounded-md p-3'}`}>Home</NavLink>
-                </button>
+            <button>
+                   <NavLink
+  to="/"
+  className={({ isActive }) =>
+    `flex items-center gap-2 ${
+      isActive
+        ? "bg-green-900 rounded-md p-3 text-amber-50"
+        : "rounded-md p-3"
+    }`
+  }
+>
+  <FaHome />
+  <span>Home</span>
+</NavLink>
+            </button>
                 <button>
            <NavLink to={'/timeline'} className={(prop)=>`${prop.isActive?'bg-green-900 rounded-md p-3  text-amber-50':'rounded-md p-3'}`}>Timeline</NavLink>
                 </button>
